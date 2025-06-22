827274704caeb3958a95bbeaf7796fc96124ae48
ShivaniNarula29 <shivaninarula9211@gmail.com>
2025-06-22 22:32:55 +0530

    Update staticcodeanalysis.groovy

--- src/org/teamdowntime/template/react/staticcodeanalysis.groovy
+++ src/org/teamdowntime/template/react/staticcodeanalysis.groovy
@@ -3,14 +3,6 @@ package org.teamdowntime.template.react
 import org.teamdowntime.common.*
 
     def call(String branch, String url, String creds, String projectKey, String sonarUrl, String sonarSources, String sonarToken) {
-        println "[INFO] --- Static Code Analysis Parameters ---"
-        println "[INFO] Branch        : ${branch}"
-        println "[INFO] Repository URL: ${url}"
-        println "[INFO] Credentials   : ${creds}"
-        println "[INFO] Sonar Project : ${projectKey}"
-        println "[INFO] Sonar URL     : ${sonarUrl}"
-        println "[INFO] Sonar Sources : ${sonarSources}"
-        println "[INFO] Sonar Token   : ${sonarToken}"
 
         def checkout = new checkout()
         def cleanworkspace = new cleanworkspace()
